ea80b27d914bb9dad965948d70f6178cd6b4ea4f
vmassey2002 <77120123+vmassey2002@users.noreply.github.com>
2024-03-16 00:48:08 -0700

    Create task1.ino

---
+++ Lab 4/sketch_mar16a/task1.ino
@@ -0,0 +1,40 @@
+#include <Arduino_LSM6DS3.h>
+
+void setup_accelerometer() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in g's");
+  Serial.println("X\tY\tZ");
+}
+
+void read_and_print_acceleration() {
+  float x, y, z;
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(y);
+    Serial.print('\t');
+    Serial.println(z);
+  }
+}
+
+void setup() {
+  setup_accelerometer();
+}
+
+void loop() {
+  read_and_print_acceleration();
+}
